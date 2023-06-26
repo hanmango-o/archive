f50f9ae7ab68d18d2fb9822d6aaddcecf951f46b
hanmango-o <hanmango.o@gmail.com>
2023-06-26 23:24:01 +0900

    init : second brain

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,8 @@
-export default {};
+const config = {
+    title: "@hanmango-o",
+    description: "Dev.HAN",
+    author: "hanmango-o",
+    navLinks: [{href: "/docs", name: "Docs"}, {href: "/blog", name: "Blog"}, {href: "/about", name: "About"}]
+};
+
+export default config;
