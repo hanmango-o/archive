eacb51a1b702db46a8b04781f4da78f917de977e
hanmango-o <hanmango.o@gmail.com>
2023-06-27 00:19:42 +0900

    add : add showEditLink config to false

--- content/config.mjs
+++ content/config.mjs
@@ -2,6 +2,7 @@ const config = {
     title: "@hanmango-o",
     description: "Dev.HAN",
     author: "hanmango-o",
+    showEditLink: false,
     navLinks: [{href: "/docs", name: "Docs"}, {href: "/blog", name: "Blog"}, {href: "/about", name: "About"}]
 };
 
